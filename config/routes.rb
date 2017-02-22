32905ccb0aefb83194e369add35bef788a7de233
deaundrapeek <jingyufanclub@gmail.com>
2017-02-21 21:54:12 -0500

    deploy to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root "welcome#index"
   namespace :api do
     namespace :v1 do
       resources :users
       resources :galleries
-      resources :pieces #, only:[]
+      resources :pieces
       post 'signup', to: 'users#create'
       post 'login', to: 'sessions#create'
       post 'remove', to: 'gallery_pieces#destroy'
